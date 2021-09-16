787eaca2875eb4d94c3cd75fb3b5c89ec034b267
muril <qiapps.contato@gmail.com>
2021-09-15 22:18:40 -0300

    alterações

---
+++ android/src/main/java/com/jimmydaddy/imagemarker/TextBackgroundStyle.java
@@ -0,0 +1,82 @@
+package com.jimmydaddy.imagemarker;
+
+import android.graphics.Color;
+import android.util.Log;
+import com.facebook.react.bridge.ReadableMap;
+
+import static com.jimmydaddy.imagemarker.Utils.transRGBColor;
+
+public class TextBackgroundStyle {
+    public String type = "";
+    public float paddingX = 0F;
+    public float paddingY = 0F;
+    public int color = Color.TRANSPARENT;
+    public int borderRadius = 0;
+
+    public TextBackgroundStyle(String type, float paddingX, float paddingY, int color,int borderRadius) {
+        this.type = type;
+        this.paddingX = paddingX;
+        this.paddingY = paddingY;
+        this.color = color;
+        this.borderRadius = borderRadius;
+    }
+
+    public TextBackgroundStyle(ReadableMap readableMap) {
+        if (null != readableMap) {
+            try {
+                this.setType(readableMap.getString("type"));                
+                this.setPaddingX((float) readableMap.getDouble("paddingX"));
+                this.setPaddingY((float) readableMap.getDouble("paddingY"));
+                this.setColor(readableMap.getString("color"));
+                this.borderRadius = readableMap.getInt("borderRadius");
+            } catch (Exception e) {
+                Log.d(Utils.TAG, "Unknown text background options ", e);
+            }
+
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public float getPaddingX() {
+        return paddingX;
+    }
+
+    public float getPaddingY() {
+        return paddingY;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setPaddingX(float paddingX) {
+        this.paddingX = paddingX;
+    }
+
+    public void setPaddingY(float paddingY) {
+        this.paddingY = paddingY;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public void setColor(String color) {
+        try {
+            Integer parsedColor = Color.parseColor(transRGBColor(color));
+            if (null != parsedColor) {
+                this.setColor(parsedColor);
+            }
+        } catch (Exception e) {
+            Log.d(Utils.TAG, "Unknown color string ", e);
+        }
+
+    }
+}
